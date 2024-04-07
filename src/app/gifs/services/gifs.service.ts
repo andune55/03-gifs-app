9ad978d5325c55932b59cb5670935e2fa48b1aa3
Agustin <latierramedia@gmail.com>
2024-04-07 19:27:35 +0200

    video 95

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,11 +1,14 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({providedIn: 'root'})
 export class GifsService {
 
   private _tagsHistory: string [] = [];
+  private apiKey:       string = 'HTDqPeDonsGHXBgtW8OnyitR3l5O7Pdh';
+  private serviceUrl:   string = 'https://api.giphy.com/v1/gifs';
 
-  constructor() { }
+  constructor( private http: HttpClient ) { }
 
   get tagsHistory(){
     return [...this._tagsHistory];
@@ -27,13 +30,27 @@ export class GifsService {
     this._tagsHistory = this._tagsHistory.splice(0,10);
   }
 
-  searchTag( tag: string ):void {
+   searchTag( tag: string ):void {
 
     if ( tag.length === 0 ) return;
     this.organizeHistory(tag);
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('q', tag)
+      .set('limit', '10')
+
+    this.http.get(`${ this.serviceUrl }/search?`, { params } )
+      .subscribe( resp => {
+
+        console.log( resp);
+
+      });
+
     //this._tagsHistory.unshift( tag );
-
-    console.log(this._tagsHistory);
-
+    //console.log(this._tagsHistory);
   }
+
+
+
 }
